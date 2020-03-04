2cc4dee0dd0d78ac435b685f7fa27faeac097014
dawright22 <53329137+dawright22@users.noreply.github.com>
2020-03-04 14:17:52 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,44 +10,44 @@ resource "azurerm_mysql_server" "mysql" {
   resource_group_name = "${azurerm_resource_group.mysql.name}"
 
   sku {
-    name     = "${var.sku_name}"
-    capacity = "${var.sku_capacity}"
-    tier     = "${var.sku_tier}"
-    family   = "${var.sku_family}"
+    name     = var.sku_name
+    capacity = var.sku_capacity
+    tier     = var.sku_tier
+    family   = var.sku_family
   }
 
   storage_profile {
-    storage_mb            = "${var.storage_mb}"
-    backup_retention_days = "${var.backup_retention_days}"
-    geo_redundant_backup  = "${var.geo_redundant_backup}"
+    storage_mb            = var.storage_mb
+    backup_retention_days = var.backup_retention_days
+    geo_redundant_backup  = var.geo_redundant_backup
   }
 
-  administrator_login          = "${var.admin_username}"
-  administrator_login_password = "${var.admin_password}"
-  version                      = "${var.mysql_server_version}"
-  ssl_enforcement              = "${var.enforce_ssl_mysql}"
+  administrator_login          = var.admin_username
+  administrator_login_password = var.admin_password
+  version                      = var.mysql_server_version
+  ssl_enforcement              = var.enforce_ssl_mysql
 }
 
 resource "azurerm_mysql_database" "mysql" {
   name                = "${var.db_name}${local.name_suffix}"
   resource_group_name = "${azurerm_resource_group.mysql.name}"
   server_name         = "${azurerm_mysql_server.mysql.name}"
-  charset             = "${var.charset}"
-  collation           = "${var.collation}"
+  charset             = var.charset
+  collation           = var.collation
 }
 
 resource "azurerm_mysql_firewall_rule" "mysql" {
   name                = "${var.firewall_rule_name}${local.name_suffix}"
   resource_group_name = "${azurerm_resource_group.mysql.name}"
   server_name         = "${azurerm_mysql_server.mysql.name}"
-  start_ip_address    = "${var.start_ip_address}"
-  end_ip_address      = "${var.end_ip_address}"
+  start_ip_address    = var.start_ip_address
+  end_ip_address      = var.end_ip_address
 }
 
 resource "azurerm_management_lock" "conditional" {
   count      = "${var.create_lock ? 1 : 0}"
   name       = "${var.resource_lock_name}${local.name_suffix}"
   scope      = "${azurerm_resource_group.mysql.id}"
-  lock_level = "${var.lock_level}"
-  notes      = "${var.notes}"
+  lock_level = var.lock_level
+  notes      = var.notes
 }
